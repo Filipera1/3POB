fb301fc32434d976d54be0a2cdf3e83f875590d7
Filipera1 <albuquerquefilips@gmail.com>
2024-11-28 03:05:07 -0300

    Update FuncionarioController.java

--- ProjetoAv2/controller/FuncionarioController.java
+++ ProjetoAv2/controller/FuncionarioController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 import model.*;
@@ -10,11 +9,11 @@ public class FuncionarioController {
     private ArrayList<Funcionario> funcionarios;
     private ArquivoController arquivoController;
     private FuncionarioView view;
-    private int proximoId; // Gerenciador de IDs únicos
+    private int proximoId; 
 
     public FuncionarioController() {
         this.arquivoController = new ArquivoController();
-        this.funcionarios = arquivoController.carregarFuncionarios(); // Carrega funcionários do arquivo
+        this.funcionarios = arquivoController.carregarFuncionarios();
         this.view = new FuncionarioView();
         this.proximoId = calcularProximoId();
     }
@@ -28,7 +27,7 @@ public class FuncionarioController {
         }
         return maiorId + 1;
     }
-
+    
     public void cadastrarFuncionario(int tipo, String nome, double salario) {
         Funcionario funcionario = null;
         switch (tipo) {
@@ -81,87 +80,3 @@ public class FuncionarioController {
         return funcionarios.stream().filter(f -> f.getId() == id).findFirst().orElse(null);
     }
 }
-=======
-package controller;
-
-import model.*;
-import view.FuncionarioView;
-
-import java.util.ArrayList;
-
-public class FuncionarioController {
-    private ArrayList<Funcionario> funcionarios;
-    private ArquivoController arquivoController;
-    private FuncionarioView view;
-    private int proximoId; // Gerenciador de IDs únicos
-
-    public FuncionarioController() {
-        this.arquivoController = new ArquivoController();
-        this.funcionarios = arquivoController.carregarFuncionarios(); // Carrega funcionários do arquivo
-        this.view = new FuncionarioView();
-        this.proximoId = calcularProximoId();
-    }
-
-    private int calcularProximoId() {
-        int maiorId = 0;
-        for (Funcionario funcionario : funcionarios) {
-            if (funcionario.getId() > maiorId) {
-                maiorId = funcionario.getId();
-            }
-        }
-        return maiorId + 1;
-    }
-
-    public void cadastrarFuncionario(int tipo, String nome, double salario) {
-        Funcionario funcionario = null;
-        switch (tipo) {
-            case 1 -> funcionario = new Desenvolvedor(nome, salario);
-            case 2 -> funcionario = new Gerente(nome, salario);
-            case 3 -> funcionario = new Treinador(nome, salario);
-            case 4 -> funcionario = new GerenteDesenvolvedor(nome, salario);
-            default -> {
-                view.mensagemErro("Tipo inválido. Funcionário não cadastrado.");
-                return;
-            }
-        }
-
-        if (funcionario != null) {
-            funcionario.setId(proximoId++);
-            funcionarios.add(funcionario);
-            arquivoController.salvarFuncionarios(funcionarios);
-            view.mensagemSucesso("Funcionário cadastrado com sucesso! ID: " + funcionario.getId());
-        }
-    }
-
-    public void listarFuncionarios() {
-        view.listarFuncionarios(funcionarios);
-    }
-
-    public void atualizarFuncionario(int id, String novoNome, double novoSalario) {
-        Funcionario funcionario = buscarFuncionarioPorId(id);
-        if (funcionario != null) {
-            funcionario.setNome(novoNome);
-            funcionario.receberAumento(novoSalario - funcionario.getSalario());
-            arquivoController.salvarFuncionarios(funcionarios);
-            view.mensagemSucesso("Funcionário atualizado com sucesso!");
-        } else {
-            view.mensagemErro("Funcionário não encontrado.");
-        }
-    }
-
-    public void excluirFuncionario(int id) {
-        Funcionario funcionario = buscarFuncionarioPorId(id);
-        if (funcionario != null) {
-            funcionarios.remove(funcionario);
-            arquivoController.salvarFuncionarios(funcionarios);
-            view.mensagemSucesso("Funcionário excluído com sucesso!");
-        } else {
-            view.mensagemErro("Funcionário não encontrado.");
-        }
-    }
-
-    private Funcionario buscarFuncionarioPorId(int id) {
-        return funcionarios.stream().filter(f -> f.getId() == id).findFirst().orElse(null);
-    }
-}
->>>>>>> 08ae04ec49415e3cd5b60ea039fe07f2f7ea8a3b
